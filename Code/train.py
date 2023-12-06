8140723f17c46278c7f0de00eda4f7486ed058d7
ChiragLakhanpal <39208607+ChiragLakhanpal@users.noreply.github.com>
2023-12-05 19:20:56 -0500

    Update train.py

--- Code/train.py
+++ Code/train.py
@@ -16,6 +16,7 @@ from tqdm.notebook import tqdm
 import subprocess
 import time
 from pathlib import Path
+from detectron2.data import transforms as T
 
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
@@ -26,11 +27,32 @@ from detectron2.utils.visualizer import ColorMode
 from detectron2.data.datasets import register_coco_instances
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
+from detectron2.data import build_detection_train_loader, build_detection_test_loader
 from detectron2.structures import Boxes, BoxMode
 import pycocotools.mask as mask_util
-
+import copy
+import detectron2.data.detection_utils as utils
 from detectron2.data import DatasetCatalog
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+import detectron2.data.detection_utils as utils
+import copy
+import torch
+import numpy as np
+
+import os
+import json
+import importlib
+import numpy as np
+import cv2
+import torch
+from detectron2.engine import DefaultPredictor
+
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+from detectron2.structures import Boxes, BoxMode
+from detectron2.config import get_cfg
+import pycocotools.mask as mask_util
 
 from pycocotools.coco import COCO
 
@@ -55,13 +77,10 @@ with open(TRAIN_ANNOTATIONS_PATH) as f:
 
 with open(VAL_ANNOTATIONS_PATH) as f:
   val_annotations_data = json.load(f)
-train_annotations_data['annotations'][0]
 
 
 category_ids = train_coco.loadCats(train_coco.getCatIds())
 category_names = [_["name_readable"] for _ in category_ids]
-
-print("## Categories\n-", "\n- ".join(category_names))
 
 no_images_per_category = {}
 
@@ -72,97 +91,6 @@ for n, i in enumerate(train_coco.getCatIds()):
 
 img_info = pd.DataFrame(train_coco.loadImgs(train_coco.getImgIds()))
 no_images_per_category = OrderedDict(sorted(no_images_per_category.items(), key=lambda x: -1*x[1]))
-
-i = 0
-for k, v in no_images_per_category.items():
-  print(k, v)
-  i += 1
-  if i > 30:
-    break
-
-"""## Data Visualisations"""
-
-fig = go.Figure([go.Bar(x=list(no_images_per_category.keys())[:50], y=list(no_images_per_category.values())[:50])])
-fig.update_layout(
-    title="No of Image per class",)
-
-fig.show()
-
-fig = go.Figure([go.Bar(x=list(no_images_per_category.keys())[50:200], y=list(no_images_per_category.values())[50:200])])
-fig.update_layout(
-    title="No of Image per class",)
-
-fig.show()
-
-fig = go.Figure([go.Bar(x=list(no_images_per_category.keys())[200:], y=list(no_images_per_category.values())[200:])])
-fig.update_layout(
-    title="No of Image per class",)
-
-fig.show()
-
-pprint(f"Average number of image per class : { sum(list(no_images_per_category.values())) / len(list(no_images_per_category.values())) }")
-pprint(f"Highest number of image per class is : { list(no_images_per_category.keys())[0]} of { list(no_images_per_category.values())[0] }")
-pprint(f"Lowest number of image per class is : Veggie Burger of { sorted(list(no_images_per_category.values()))[0] }")
-
-fig = go.Figure(data=[go.Pie(labels=list(no_images_per_category.keys())[:50], values=list(no_images_per_category.values())[:50],
-                             hole=.3, textposition='inside', )], )
-fig.update_layout(
-    title="No of Image per class Top 50 ( In pie )",)
-fig.show()
-
-fig = go.Figure()
-fig.add_trace(go.Histogram(x=img_info['height'],text='height'))
-fig.add_trace(go.Histogram(x=img_info['width'],text='width'))
-
-fig.update_layout(barmode='stack', title="Histogram of Image width & height",)
-
-
-fig.show()
-
-
-print(img_info)
-print(img_info.describe())
-
-len(train_annotations_data['annotations'][2]['segmentation']), len(train_annotations_data['annotations'][2]['bbox'])
-
-img_no = 11
-
-annIds = train_coco.getAnnIds(imgIds=train_annotations_data['images'][img_no]['id'])
-anns = train_coco.loadAnns(annIds)
-
-plt.imshow(plt.imread(TRAIN_IMAGE_DIRECTIORY+train_annotations_data['images'][img_no]['file_name']))
-plt.axis('off')
-train_coco.showAnns(anns)
-
-w, h = 15, 15 
-rows, cols = 5, 5 
-
-fig = plt.figure(figsize=(15, 15)) 
-
-plt.title("Images")
-plt.axis('off')
-
-
-for i in range(1, cols * rows+1):
-  annIds = train_coco.getAnnIds(imgIds=img_info['id'][i])
-  anns = train_coco.loadAnns(annIds)
-
-  fig.add_subplot(rows, cols, i)
-
-
-  img = plt.imread(TRAIN_IMAGE_DIRECTIORY+img_info['file_name'][i])
-  for i in anns:
-    [x,y,w,h] = i['bbox']
-    cv2.rectangle(img, (int(x), int(y)), (int(x+h), int(y+w)), (255,0,0), 2)
-  plt.imshow(img)
-
-  train_coco.showAnns(anns)
-
-  plt.axis("off")
-
-plt.show()
-
-#!pip install --upgrade fastai
 
 from fastai.vision.core import *
 from fastai.vision.utils import *
@@ -185,6 +113,86 @@ coco_tdl = TfmdDL(coco_dsrc, bs=9, after_item=[BBoxLabeler(), PointScaler(), ToT
 coco_tdl.show_batch(max_n=9)
 
 
+class DataPreprocessor:
+    def __init__(self, train_annotations_path, val_annotations_path, image_directory):
+        self.train_annotations_path = train_annotations_path
+        self.val_annotations_path = val_annotations_path
+        self.image_directory = image_directory
+        self.load_data()
+
+    def load_data(self):
+        with open(self.train_annotations_path) as f:
+            self.train_annotations_data = json.load(f)
+        with open(self.val_annotations_path) as f:
+            self.val_annotations_data = json.load(f)
+
+    def update_image_dimensions(self, annotations_data):
+        for image_data in tqdm(annotations_data['images'], desc='Updating image dimensions'):
+            file_path = os.path.join(self.image_directory, image_data["file_name"])
+            if os.path.exists(file_path):
+                img = cv2.imread(file_path)
+                image_data['height'], image_data['width'] = img.shape[:2]
+        return annotations_data
+
+    def remove_rotated_annotations(self, annotations_data):
+        annotations_data['annotations'] = [annotation for annotation in annotations_data['annotations'] if
+                                           annotation['bbox_mode'] != BoxMode.XYWHA_ABS]
+        return annotations_data
+      
+    def adjust_bounding_boxes(self, annotations_data):
+        for annotation in tqdm(annotations_data['annotations'], desc='Adjusting bounding boxes'):
+            if annotation['bbox_mode'] == BoxMode.XYWH_ABS:
+                annotation['bbox_mode'] = BoxMode.XYXY_ABS
+                x, y, w, h = annotation['bbox']
+                annotation['bbox'] = [x, y, x + w, y + h]
+        return annotations_data
+    
+    def remove_non_food_items(self, annotations_data):
+        annotations_data['annotations'] = [annotation for annotation in annotations_data['annotations'] if
+                                           annotation['category_id'] in self.food_category_ids]
+        return annotations_data
+      
+
+    def save_data(self, annotations_data, path):
+        with open(path, 'w') as f:
+            json.dump(annotations_data, f)
+
+    def process_data(self):
+        self.train_annotations_data = self.update_image_dimensions(self.train_annotations_data)
+        self.train_annotations_data = self.remove_rotated_annotations(self.train_annotations_data)
+        self.train_annotations_data = self.adjust_bounding_boxes(self.train_annotations_data)
+        
+        self.val_annotations_data = self.update_image_dimensions(self.val_annotations_data)
+        self.val_annotations_data = self.remove_rotated_annotations(self.val_annotations_data)
+        self.val_annotations_data = self.adjust_bounding_boxes(self.val_annotations_data)
+
+        self.save_data(self.train_annotations_data, 'new_ann_train.json')
+        self.save_data(self.val_annotations_data, 'new_ann_val.json')
+
+
+from fastai.vision.augment import aug_transforms, RandomErasing
+
+
+transforms = aug_transforms(
+    max_rotate=30, 
+    max_zoom=1.5,  
+    max_lighting=0.4,
+    max_warp=0.4,  
+    p_affine=0.75, 
+    p_lighting=0.75, 
+    xtra_tfms=[RandomErasing(p=0.5, max_count=2)] 
+)
+
+coco_tdl = TfmdDL(
+    coco_dsrc, 
+    bs=9, 
+    after_item=[BBoxLabeler(), PointScaler(), ToTensor(), Resize(256)],
+    after_batch=[IntToFloatTensor(), *transforms]
+)
+
+coco_tdl.show_batch(max_n=9)
+
+  
 np.array(train_annotations_data['annotations'][2]['segmentation']).shape , np.array(train_annotations_data['annotations'][2]['bbox']).shape
 
 def fix_data(annotations, directiory, VERBOSE = False):
@@ -244,26 +252,19 @@ cfg.DATALOADER.NUM_WORKERS = 8
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(MODEL_ARCH)
 
 
-# No. of Batchs
 cfg.SOLVER.IMS_PER_BATCH = 8     
 
-# Learning Rate:
-cfg.SOLVER.BASE_LR = 0.0025
+cfg.SOLVER.BASE_LR = 0.00025
 
-# No of Interations
-cfg.SOLVER.MAX_ITER = 1000
+cfg.SOLVER.MAX_ITER = 50000
 
-# Options: WarmupMultiStepLR, WarmupCosineLR.
-# See detectron2/solver/build.py for definition.
+
 cfg.SOLVER.LR_SCHEDULER_NAME = "WarmupMultiStepLR"
 
-#save every 1000 steps
-cfg.SOLVER.CHECKPOINT_PERIOD = 1000
+cfg.SOLVER.CHECKPOINT_PERIOD = 20000
 
-# Images per batch (Batch Size)
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
 
-# No of Categories(Classes) present
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 498
 
 
@@ -271,8 +272,9 @@ cfg.OUTPUT_DIR = "logs_detectron2_r50"
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 
-#set if to true if you want to resume training
-RESUME = True
+RESUME = False
+
+
 trainer = DefaultTrainer(cfg)
 
 if RESUME:
@@ -286,17 +288,15 @@ trainer.train()
 
 os.system("cp logs_detectron2_r50/model_final.pth model_final.pth")
 
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1
-
-# cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 cfg.MODEL.WEIGHTS = 'model_final.pth'
 
 evaluator = COCOEvaluator("validation_dataset", cfg, False, output_dir=cfg.OUTPUT_DIR)
 val_loader = build_detection_test_loader(cfg, "validation_dataset")
 valResults = inference_on_dataset(trainer.model, val_loader, evaluator)
 
-# cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.2
+print("Validation mAP: ", valResults["bbox"]["AP"])
+
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 498
 
 cfg.DATASETS.TEST = ("validation_dataset", )
@@ -304,29 +304,22 @@ predictor = DefaultPredictor(cfg)
 
 val_metadata = MetadataCatalog.get("val_dataset")
 
-# Sample image
 image_id = '183370'
 im = cv2.imread(f"data/val/images/{image_id}.jpg")
 outputs = predictor(im)
 
-# Convert BGR to RGB (for display purposes)
 im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
-# Extract bounding boxes and labels
 boxes = outputs["instances"].pred_boxes.tensor.cpu().numpy()
 classes = outputs["instances"].pred_classes.cpu().numpy()
 scores = outputs["instances"].scores.cpu().numpy()
 
-# Convert image to PIL format for compatibility with Plotly
 im_pil = Image.fromarray(im)
 
-# Create a figure
 fig = go.Figure()
 
-# Add the image
 fig.add_trace(go.Image(z=im_pil))
 
-# Add each bounding box as a rectangle
 for box, cls, score in zip(boxes, classes, scores):
     y1, x1, y2, x2 = box
     fig.add_shape(
@@ -334,7 +327,6 @@ for box, cls, score in zip(boxes, classes, scores):
         x0=x1, y0=y1, x1=x2, y1=y2,
         line=dict(color="Red"),
     )
-    # Optionally add label and score
     fig.add_trace(go.Scatter(
         x=[x1], y=[y1],
         text=[f"{cls} ({score:.2f})"],
@@ -342,17 +334,14 @@ for box, cls, score in zip(boxes, classes, scores):
         textposition="bottom center"
     ))
 
-# Update layout
 fig.update_layout(
     margin=dict(l=0, r=0, t=0, b=0),
     width=im.shape[1],
     height=im.shape[0],
 )
 
-# Show the figure
 fig.show()
 
-#generate class category ids from detectron2 internal dictinary mappings
 category_ids = sorted(train_coco.getCatIds())
 categories = train_coco.loadCats(category_ids)
 
@@ -362,91 +351,14 @@ with open("class_to_category.json", "w") as fp:
   json.dump(class_to_category, fp)
 
 
-#setting the paths and threshold
-test_images_dir = "./data/test/images"
+test_images_dir = "data/test/images"
 output_filepath = "predictions_detectron2.json"
 
-#path of trained model
 model_path = 'model_final.pth'
-# model_path = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 
-#threshold
 threshold = 0.1
-
-import os
-import json
-# import aicrowd_helpers
-import importlib
-import numpy as np
-import cv2
-import torch
-from detectron2.engine import DefaultPredictor
-
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
-from detectron2.structures import Boxes, BoxMode
-from detectron2.config import get_cfg
-import pycocotools.mask as mask_util
 
 
 class_to_category = {}
 with open("class_to_category.json") as fp:
     class_to_category = json.load(fp)
-
-
-def run():
-    model_name = "model_zoo"
-    model = importlib.import_module(f"detectron2.{model_name}")
-
-    #set the config parameters, including the architecture which was previously used
-    cfg = get_cfg()
-    cfg.merge_from_file(model.get_config_file(MODEL_ARCH))
-    cfg.MODEL.WEIGHTS = model_path
-
-    #set the threshold
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold   # set the testing threshold for this model
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 498
-
-    cfg.MODEL.DEVICE = "cuda"
-    predictor = DefaultPredictor(cfg)
-
-    results = []
-    for img_file in tqdm(os.listdir(test_images_dir)):
-        filename = os.path.join(test_images_dir, img_file)
-        img = cv2.imread(filename)
-        prediction = predictor(img)
-
-        instances = prediction["instances"]
-        if len(instances) > 0:
-            scores = instances.scores.tolist()
-            classes = instances.pred_classes.tolist()
-            bboxes = BoxMode.convert(
-                instances.pred_boxes.tensor.cpu(),
-                BoxMode.XYXY_ABS,
-                BoxMode.XYWH_ABS,
-            ).tolist()
-
-            masks = []
-            if instances.has("pred_masks"):
-                for mask in instances.pred_masks.cpu():
-                    _mask = mask_util.encode(np.array(mask[:, :, None], order="F", dtype="uint8"))[0]
-                    _mask["counts"] = _mask["counts"].decode("utf-8")
-                    masks.append(_mask)
-
-            for idx in range(len(instances)):
-                category_id = class_to_category[str(classes[idx])] # json converts int keys to str
-                output = {
-                    "image_id": int(img_file.split(".")[0]),
-                    "category_id": category_id,
-                    "bbox": bboxes[idx],
-                    "score": scores[idx],
-                }
-                if len(masks) > 0:
-                    output["segmentation"] = masks[idx]
-                results.append(output)
-
-        with open(output_filepath, "w") as fp:
-            json.dump(results, fp)
-
-
-run()
